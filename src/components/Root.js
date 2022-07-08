40ee4728e0a566a925cde3f835c29f01ab73b63a
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2022-07-08 21:55:56 +0545

    Cards

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Cards from "./Cards"
 import Search from "./Search"
 import Tabs from "./Tabs"
 import Time from "./Time"
@@ -12,8 +13,9 @@ const Root = () => {
 			<Tabs />
 			{/* Weather */}
 			<div className="item weather">Weather</div>
-			{/* Childrens */}
-			<div className="item childrens">Childrens</div>
+			{/* Cards */}
+			<Cards />
+			{/* <div className="item childrens">Childrens</div> */}
 			{/* Search */}
 			<Search />
 			{/* <div className="item search">Search</div> */}
