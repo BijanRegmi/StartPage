f922843e9736b0c2c10fec07687d6f03917ebd27
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2022-07-08 19:25:11 +0545

    Component based approach

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Provider } from "react-redux"
+import Root from "./components/Root"
 import Viewport from "./components/Viewport"
 import store from "./StateManagement/store"
 
 export default () => (
 	<Provider store={store}>
-		<Viewport />
+		<Root />
 	</Provider>
 )
---
+++ src/components/Time.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react"
+import "../Styles/Time.css"
+
+const Time = () => {
+	const [currDate, setDate] = useState(new Date())
+
+	useEffect(() => {
+		setInterval(() => setDate(new Date()), 1000)
+	}, [])
+
+	return (
+		<div className="item time">
+			<p>
+				{currDate.toLocaleString("en-US", {
+					hour: "2-digit",
+					minute: "2-digit",
+					second: "2-digit",
+				})}
+			</p>
+		</div>
+	)
+}
+
+export default Time
