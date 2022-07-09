d8a0271e23416b0209951842c440d7c5ba9ba612
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2022-07-09 21:04:21 +0545

    okay

--- src/StateManagement/store.js
+++ src/StateManagement/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import thunk from "redux-thunk"
 import rootReducer from "./rootReducer"
-import searchReducer from "./searchReducer"
 
 const store = configureStore({
 	reducer: {
 		root: rootReducer,
-		search: searchReducer,
 	},
 	middleware: [thunk],
 })
