feab85593ebb724cca26cff319bf6e1a1b7070cd
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2022-07-09 13:17:26 +0545

    hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./Styles/app.css"
+import { Provider } from "react-redux"
+import store from "./StateManagement/store"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+)
