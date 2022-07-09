aa946a360c2e747975dd0adf0d0a478317c4d89e
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2022-07-09 13:17:26 +0545

    hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import { Provider } from "react-redux"
-import store from "./StateManagement/store"
-
 import Cards from "./components/Cards"
 import Search from "./components/Search"
 import Tabs from "./components/Tabs"
 import Time from "./components/Time"
 
-export default () => (
-	<Provider store={store}>
-		<Time />
-		<Tabs />
-		<div className="item weather">Weather</div>
-		<Cards />
-		<Search />
-		<div className="item cmd">CMD</div>
-	</Provider>
-)
+export default () => {
+	return (
+		<div className="startpage">
+			<Time />
+			<Tabs />
+			<div className="item weather">Weather</div>
+			<Cards />
+			<Search />
+			<div className="item cmd">CMD</div>
+		</div>
+	)
+}
